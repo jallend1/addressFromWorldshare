7c2f8d1942e4edbe2076ea4712cf39b47dcbfcc6
Jason Allen <39307371+jallend1@users.noreply.github.com>
2023-07-30 12:05:58 -0700

    Adds state abbreviation converter and styles appropriately for mailing label

--- copyAddress.js
+++ copyAddress.js
@@ -1,33 +1,128 @@
 javascript: (function () {
-  let attentionField = document.querySelectorAll(
-  '[data="returning.address.attention"]'
-)[0].value;
-let addressLineOne = document.querySelectorAll(
-  '[data="returning.address.line1"]'
-)[0].value;
-let addressLineTwo = document.querySelectorAll(
-  '[data="returning.address.line2"]'
-)[0].value;
-let city = document.querySelectorAll('[data="returning.address.locality"]')[0]
-  .value;
-let state = document
-  .querySelectorAll('[data="returning.address.region"]')[0]
-  .value.split('_')[1];
-let zip = document.querySelectorAll('[data="returning.address.postal"]')[0]
-  .value;
+  let addressObject = {
+    attention: null,
+    line1: null,
+    line2: null,
+    locality: null,
+    region: null,
+    postal: null
+  };
 
-navigator.clipboard.writeText(
-  attentionField +
-    '\n' +
-    addressLineOne +
-    '\n' +
-    addressLineTwo +
-    '\n' +
-    city +
-    ', ' +
-    state +
-    ' ' +
-    zip
-);
-}
-)();
+  const convertStateNameToAbbreviation = (stateName) => {
+    const states = {
+      Alabama: 'AL',
+      Alaska: 'AK',
+      'American Samoa': 'AS',
+      Arizona: 'AZ',
+      Arkansas: 'AR',
+      California: 'CA',
+      Colorado: 'CO',
+      Connecticut: 'CT',
+      Delaware: 'DE',
+      'District Of Columbia': 'DC',
+      'Federated States Of Micronesia': 'FM',
+      Florida: 'FL',
+      Georgia: 'GA',
+      Guam: 'GU',
+      Hawaii: 'HI',
+      Idaho: 'ID',
+      Illinois: 'IL',
+      Indiana: 'IN',
+      Iowa: 'IA',
+      Kansas: 'KS',
+      Kentucky: 'KY',
+      Louisiana: 'LA',
+      Maine: 'ME',
+      'Marshall Islands': 'MH',
+      Maryland: 'MD',
+      Massachusetts: 'MA',
+      Michigan: 'MI',
+      Minnesota: 'MN',
+      Mississippi: 'MS',
+      Missouri: 'MO',
+      Montana: 'MT',
+      Nebraska: 'NE',
+      Nevada: 'NV',
+      'New Hampshire': 'NH',
+      'New Jersey': 'NJ',
+      'New Mexico': 'NM',
+      'New York': 'NY',
+      'North Carolina': 'NC',
+      'North Dakota': 'ND',
+      'Northern Mariana Islands': 'MP',
+      Ohio: 'OH',
+      Oklahoma: 'OK',
+      Oregon: 'OR',
+      Palau: 'PW',
+      Pennsylvania: 'PA',
+      'Puerto Rico': 'PR',
+      'Rhode Island': 'RI',
+      'South Carolina': 'SC',
+      'South Dakota': 'SD',
+      Tennessee: 'TN',
+      Texas: 'TX',
+      Utah: 'UT',
+      Vermont: 'VT',
+      'Virgin Islands': 'VI',
+      Virginia: 'VA',
+      Washington: 'WA',
+      'West Virginia': 'WV',
+      Wisconsin: 'WI',
+      Wyoming: 'WY'
+    };
+    return states[stateName];
+  };
+
+  const assignAddressObjectValues = (key) => {
+    if (key === 'region') {
+      let nodeList = document.querySelectorAll(
+        'span[data="returning.address.region"]'
+      );
+      nodeList.length > 0
+        ? (addressObject[key] = convertStateNameToAbbreviation(
+            nodeList[nodeList.length - 1].innerText
+          ))
+        : (addressObject[key] = 'NONE');
+    } else {
+      let nodeList = document.querySelectorAll(
+        `input[data="returning.address.${key}"]`
+      );
+      nodeList.length > 0
+        ? (addressObject[key] = nodeList[nodeList.length - 1].value)
+        : (addressObject[key] = null);
+    }
+  };
+
+  // Iterate through addressObject keys and extract values from page
+  Object.keys(addressObject).forEach(assignAddressObjectValues);
+
+  // Format addressObject for mail label
+  const createAddressString = () => {
+    let addressString = '';
+    Object.keys(addressObject).forEach((key) => {
+      switch (key) {
+        case 'attention':
+        case 'line1':
+        case 'line2':
+          addressString += addressObject[key] + '\n';
+          break;
+        case 'locality':
+          addressString += addressObject[key] + ', ';
+          break;
+        case 'region':
+          addressString += addressObject[key] + ' ';
+          break;
+        case 'postal':
+          addressString += addressObject[key];
+          break;
+        default:
+          break;
+      }
+    });
+    return addressString;
+  };
+
+  const addressString = createAddressString();
+
+  navigator.clipboard.writeText(addressString);
+})();
